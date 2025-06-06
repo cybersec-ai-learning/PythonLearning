60c47c0432888f4cf1525f7c6815db6ce361dd17
Cybersec AI Learning <50544080+cybersec-ai-learning@users.noreply.github.com>
2025-06-06 14:04:56 -0500

    Update lists.py

---
+++ lists.py
@@ -0,0 +1,5 @@
+flowers_list = ["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold", "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle"]
+
+print(type(flowers_list))
+print(flowers_list)
+print(flowers_list[2])
