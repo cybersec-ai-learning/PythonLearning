15a6135c87c0b452f5678abfa9d1a99b751e7b80
Cybersec AI Learning <50544080+cybersec-ai-learning@users.noreply.github.com>
2025-06-06 12:56:08 -0500

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -4,6 +4,7 @@ var_two = 2
 print(var_one < 1)
 print(var_two >= var_one)
 
+#if condition
 def evaluate_temp(temp):
     # Set an initial message
     message = "Normal temperature."
@@ -11,3 +12,24 @@ def evaluate_temp(temp):
     if temp > 38:
         message = "Fever!"
     return message
+print(evaluate_temp(39))
+
+#if-else conditions
+def evaluate_temp_with_else(temp):
+    if temp > 38:
+        message = "Fever!"
+    else:
+        message = "Normal temperature."
+    return message
+print(evaluate_temp_with_else(30))
+
+#if-elif-else conditions
+def evaluate_temp_with_elif(temp):
+    if temp > 38:
+        message = "Fever!"
+    elif temp > 35:
+        message = "Normal temperature."
+    else:
+        message = "Low temperature."
+    return message
+print(evaluate_temp_with_elif(20))
