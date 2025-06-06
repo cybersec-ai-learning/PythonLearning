93447a713a6b38f6204a36d9c0f4748f1f45677c
Cybersec AI Learning <50544080+cybersec-ai-learning@users.noreply.github.com>
2025-06-06 10:20:39 -0500

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,43 @@
+#Addition
+print(2+4)
+#Multiplication
+print(2*4)
+#Subtraction
+print(2-4)
+#Exponent
+print(2**4)
+#Modulus
+print(2%4)
+#Division
+print(2/4)
+
+#assign variable
+test_var = 2+4
+#print variable
+print(test_var)
+
+#Variable manipulation
+my_var = 3
+print(my_var)
+my_var = 100
+print(my_var)
+
+#Increment Variable
+my_var = my_var + 3
+print(my_var)
+
+# Create Time Variables
+num_years = 4
+days_per_year = 365 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
+
+# Update to include leap years
+days_per_year = 365.25
+
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
